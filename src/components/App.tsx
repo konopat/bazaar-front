1aee9a8dbe87a88d9d8abce944c29636cdc1c674
Роман Предеин <konopat@Romans-MacBook-Pro.local>
2025-03-30 01:49:02 +0800

    feat: оптимизировал роутинг

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,7 +21,7 @@ const ProductPage = lazy(() => import('./pages/ProductPage'));
 
 const App = () => {
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ScrollToTop />
       <div className="app">
         <Header />
